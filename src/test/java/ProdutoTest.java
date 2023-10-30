c1a9b28f26ab0efde575eba0af630807ee5b630b
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-10-30 18:17:12 -0300

    fix test 1

--- src/test/java/ProdutoTest.java
+++ src/test/java/ProdutoTest.java
@@ -5,23 +5,28 @@ import static org.junit.Assert.assertNotNull;
 import main.IProdutoDAO;
 import main.java.Produto;
 import main.java.ProdutoDAO;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProdutoTest {
 
-  @Test
-  public void Read_Test() {
+  public Produto criarProdutoTeste() {
     Produto produto = new Produto();
     produto.setCodigo("123");
     produto.setNome("aksjdhaskdj");
     produto.setDescricao("lorem");
     produto.setValor(10D);
     produto.setFabricante("qoqlqlalalqoqla");
+    return produto;
+  }
 
+  @Test
+  public void Read_Test() {
     IProdutoDAO produtoDAO = new ProdutoDAO();
 
+    Produto produto = criarProdutoTeste();
     produtoDAO.create(produto);
-    Produto result = produtoDAO.read(produto.getId())
+    Produto result = produtoDAO.read(produto.getId());
     assertNotNull(result);
   }
 }
