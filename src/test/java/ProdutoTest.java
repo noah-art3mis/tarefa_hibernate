f6110f8287f5f8cf7151fa7b4fed208b14233fb2
noah-art3mis <costa.araujo.gustavo@gmail.com>
2023-11-01 09:47:52 -0300

    add produto tests

--- src/test/java/ProdutoTest.java
+++ src/test/java/ProdutoTest.java
@@ -1,7 +1,13 @@
 package test.java;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.Random;
 import main.IProdutoDAO;
 import main.java.Produto;
 import main.java.ProdutoDAO;
@@ -10,9 +16,13 @@ import org.junit.Test;
 
 public class ProdutoTest {
 
+  public String chaveAleatoria() {
+    return Integer.toString((new Random().nextInt(10000)));
+  }
+
   public Produto criarProdutoTeste() {
     Produto produto = new Produto();
-    produto.setCodigo("123");
+    produto.setCodigo(chaveAleatoria());
     produto.setNome("aksjdhaskdj");
     produto.setDescricao("lorem");
     produto.setValor(10D);
@@ -21,12 +31,45 @@ public class ProdutoTest {
   }
 
   @Test
-  public void Read_Test() {
+  public void readAndCreate_Test() {
     IProdutoDAO produtoDAO = new ProdutoDAO();
 
     Produto produto = criarProdutoTeste();
     produtoDAO.create(produto);
     Produto result = produtoDAO.read(produto.getId());
     assertNotNull(result);
+  }
+
+  @Test
+  public void update_Test() {
+    IProdutoDAO produtoDAO = new ProdutoDAO();
+
+    Produto produto = criarProdutoTeste();
+    produtoDAO.create(produto);
+    Produto produto2 = criarProdutoTeste();
+    produto2.setNome("Novo Nome");
+    Produto produto3 = produtoDAO.update(produto2);
+    assertEquals("Novo Nome", produto3.getNome());
+  }
+
+  @Test
+  public void delete_Test() {
+    IProdutoDAO produtoDAO = new ProdutoDAO();
+
+    Produto produto = criarProdutoTeste();
+    produtoDAO.create(produto);
+    produtoDAO.delete(produto);
+    assertNull(produtoDAO.read(produto.getId()));
+  }
+
+  @Test
+  public void readAll_Test() {
+    IProdutoDAO produtoDAO = new ProdutoDAO();
+
+    Produto produto = criarProdutoTeste();
+    produtoDAO.create(produto);
+    List<Produto> list = produtoDAO.readAll();
+    // assertTrue(list.contains(produtoDAO.read()));
+    assertNotEquals(0, list.size());
   }
 }
